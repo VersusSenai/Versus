3cac1425f2013680b8106a623076cba849ac7d64
guiqsassi <guiqsassi@gmail.com>
2025-03-27 20:56:44 -0300

    feat/ user service and user basic routes

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ import connection from "./config/connection.js";
 import express from "express";
 import mysql  from "mysql2";
 import 'dotenv/config';
+import router from "./routes/router.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.get('/some-route', (req, res) => {
   res.send('Route works!');
 });
+app.use(router)
 
 app.listen(process.env.PORT, () => { console.log('connect: ', process.env.PORT) });
