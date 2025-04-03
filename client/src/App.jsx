16fcd7ffa8b98948ae36ab208d38c6f1917f7e51
Eduardo <riuugamer777@gmail.com>
2025-04-02 22:08:53 -0300

    feat(login): login screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import Register from "./pages/Register.jsx";
+import Homepage from "./pages/Homepage.jsx";
 
 function App() {
-  return <Login />;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/homepage" element={<Homepage />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
