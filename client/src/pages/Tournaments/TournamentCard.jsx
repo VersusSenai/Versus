468379b71fbfc316941106cebb974dd25e836f21
Guilherme Queiroz Sassi <guilherme.sassi@udesc.br>
2025-10-24 14:33:56 -0300

    fix/ evento modal open

--- client/src/pages/Tournaments/TournamentCard.jsx
+++ client/src/pages/Tournaments/TournamentCard.jsx
@@ -4,11 +4,21 @@ import Tag from './Tag';
 import defaultTournamentImage from '../../assets/solo.jpg';
 import { formatDate } from '../../utils/formatDate';
 
-export default function TournamentCard({ event, isInscribed, eventStatus, winnerName, matches }) {
+export default function TournamentCard({
+  event,
+  isInscribed,
+  eventStatus,
+  winnerName,
+  matches,
+  ...props
+}) {
   const hasMatches = matches.length > 0;
 
   return (
-    <Card className="cursor-pointer pt-0 hover:shadow-lg transition-shadow flex flex-col bg-[var(--color-dark)] text-white border border-white/10 rounded-2xl shadow-md select-none">
+    <Card
+      className="cursor-pointer pt-0 hover:shadow-lg transition-shadow flex flex-col bg-[var(--color-dark)] text-white border border-white/10 rounded-2xl shadow-md select-none"
+      {...props}
+    >
       <div className="w-full h-48 overflow-hidden rounded-t-2xl bg-gray-800">
         <img
           src={event.thumbnail ? event.thumbnail : defaultTournamentImage}
